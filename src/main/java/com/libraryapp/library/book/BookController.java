41dfe5fbe8eb70ccbc2d646d11dbeac3effd69c3
Kornel <kornel.brenkacz@gmail.com>
2019-01-15 21:41:12 +0100

    New: Swagger, method to test Swagger that returns all about class Book

--- src/main/java/com/libraryapp/library/book/BookController.java
+++ src/main/java/com/libraryapp/library/book/BookController.java
@@ -1,7 +1,13 @@
 package com.libraryapp.library.book;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -12,5 +18,11 @@ public class BookController {
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Book>> getBooks() {
+
+        return new ResponseEntity<List<Book>>(HttpStatus.OK);
     }
 }
